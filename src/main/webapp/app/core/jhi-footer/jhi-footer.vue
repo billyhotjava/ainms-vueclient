576254a09fc231f4dac330815825de6cfecbd5dc
billyhotjava <billy.xiezhimin@gmail.com>
2024-04-01 12:51:33 +0800

    update

--- src/main/webapp/app/core/jhi-footer/jhi-footer.vue
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.vue
@@ -1,7 +1,22 @@
 <template>
-  <div id="footer" class="footer">
+  <div id="footer ly-footer" class="footer">
     <p v-text="t$('footer')"></p>
   </div>
 </template>
 
 <script lang="ts" src="./jhi-footer.component.ts"></script>
+
+<style scoped>
+.ly-footer{
+  background-color: #353d47;
+}
+
+.footer {
+  height: 10vh;
+}
+
+.footer p {
+  text-align: center;
+  line-height: 10vh;
+}
+</style>
